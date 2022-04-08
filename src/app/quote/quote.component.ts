c2b4178f4eb9f33719abd7ae0b8760823d33f2bc
Kimberly <kimolanga123@gmail.com>
2022-04-09 01:17:22 +0300

    add constractor

--- src/app/quote/quote.component.ts
+++ src/app/quote/quote.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { Quote } from '../quote';
 
 @Component({
   selector: 'app-quote',
@@ -6,6 +7,13 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./quote.component.css']
 })
 export class QuoteComponent implements OnInit {
+  quotes: Quote[] =[
+    new Quote(1, 'Early to bed and early to rise', 0, 0),
+    new Quote(2, 'An apple a day keeps the doctor away', 0, 0),
+    new Quote(3, 'The greatest pressure in life is from within', 0, 0),
+    new Quote(4, 'Beauty lies in the eyes of the beholder', 0, 0),
+    new Quote(5, 'It is what it is', 0, 0)
+  ]
 
   constructor() { }
 
