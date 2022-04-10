aea728b76690189873cf9f6d4e4028212d1b67f6
Kimberly <kimolanga123@gmail.com>
2022-04-10 23:11:18 +0300

    add pipe directive

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -9,6 +9,8 @@ import { FormsModule } from '@angular/forms';
 import { QuoteDetailComponent } from './quote-detail/quote-detail.component';
 import { HighlightDirective } from './highlight.directive';
 import { VoteDirective } from './vote.directive';
+import { DateDirective } from './date.directive';
+import { DateCountPipe } from './date-count.pipe';
 
 @NgModule({
   declarations: [
@@ -17,7 +19,9 @@ import { VoteDirective } from './vote.directive';
     QuoteFormComponent,
     QuoteDetailComponent,
     HighlightDirective,
-    VoteDirective
+    VoteDirective,
+    DateDirective,
+    DateCountPipe
   ],
   imports: [
     BrowserModule,
